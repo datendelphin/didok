18d242b2d15a3d42d4cb25837f895c6dce50ca92
Michael Spreng <code@datendelphin.net>
2014-05-06 20:24:47 +0200

    use new django DetailView

--- django/src/didok/stopview/urls.py
+++ django/src/didok/stopview/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 #from stopview.models import *
 from didok.stopview.models import *
+
+from django.views.generic.detail import DetailView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -20,10 +22,10 @@ urlpatterns = patterns('didok.stopview.views',
 )
 
 urlpatterns += patterns('',
-    (r'^info/d(?P<object_id>\d+)$', 'django.views.generic.list_detail.object_detail',
-         {'queryset' : DIDOKStops.objects.all(),
-          'template_name' : 'vector/didok_info.html'}),
-    (r'^info/o(?P<object_id>\d+)$', 'django.views.generic.list_detail.object_detail',
-         {'queryset' : OSMStops.objects.all(),
-          'template_name' : 'vector/osm_info.html'}),
+    (r'^info/d(?P<pk>\d+)$', DetailView.as_view(
+           queryset = DIDOKStops.objects.all(),
+           template_name = 'vector/didok_info.html')),
+    (r'^info/o(?P<pk>\d+)$', DetailView.as_view(
+           queryset = OSMStops.objects.all(),
+           template_name = 'vector/osm_info.html')),
 )
